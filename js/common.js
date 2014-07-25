c5bb6fb60f3eb718b84584ba6e1aceed6f201b25
akella <akella.a@gmail.com>
2014-07-25 13:36:21 +0400

    done

--- js/common.js
+++ js/common.js
@@ -3,6 +3,8 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
-
+	$(".header__menu").click(function(event) {
+		$('.menu').slideToggle();
+	});
 	console.log($('body').html());
 });
