350187d54717d23f0aa505fdb1af9cdaa8cc787d
akella <akella.a@gmail.com>
2014-07-28 16:08:13 +0300

    test

--- js/common.js
+++ js/common.js
@@ -14,5 +14,35 @@ head.ready(function() {
 			$(this).addClass('is-active').next().slideDown();
 		}
 	});
-	console.log($('body').html());
+
+
+
+	function calcResults(){
+			
+	        var count = 0;
+	        if($('.test :checked').length == 5){
+
+	            $('.test :checked').each(function() {
+	                count += parseInt($(this).attr('value'));
+	            });
+	            $('.results-list > div').hide();
+	            if (count >= 0 && count <= 10) {
+	                $('.result-1').show();
+	            } else if (count >= 11 && count <= 15) {
+	                $('.result-2').show();
+	            } else if (count >= 16 && count <= 20) {
+	                $('.result-3').show();
+	            } else if (count >= 21 && count <= 25) {
+	                $('.result-4').show();
+	            }
+	        }
+	        else{
+	        	alert('Пожалуйста, ответьте на все вопросы');
+	        }
+	    }
+
+	    $('.test__submit a').on('click', function(){
+	        calcResults();
+	        return false;
+	    });
 });
