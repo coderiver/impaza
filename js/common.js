183d9740dcfeb31c252db7e3677065991198cf6e
akella <akella.a@gmail.com>
2014-07-28 15:35:35 +0300

    done

--- js/common.js
+++ js/common.js
@@ -6,5 +6,13 @@ head.ready(function() {
 	$(".header__menu").click(function(event) {
 		$('.menu').slideToggle();
 	});
+
+	$(".toggle dt").click(function(event) {
+		if(!$(this).hasClass('is-active')){
+			$('.toggle dt').removeClass('is-active');
+			$('.toggle dd').slideUp();
+			$(this).addClass('is-active').next().slideDown();
+		}
+	});
 	console.log($('body').html());
 });
